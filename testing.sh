d227f82a27ef65009faa7b3cb06a19013a2462c1
Zakys98 <zakys98@gmail.com>
2020-09-17 23:14:13 +0200

    test merge

--- testing.sh
+++ testing.sh
@@ -144,3 +144,5 @@ diff -s $EXPsectionTextDoesntExist <(./owo delete 1 0)
 ./owo add git "git switch <branch>                  -   ---||---"
 ./owo add git "git merge <branch>                   - copy branch to master"
 ./owo add git "git rebase <branch>                  - copy branch to master and destroy this branch"
+
+./owo
